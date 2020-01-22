3e588d74cb3a9279390eba4d805213eadec6695f
Phil Austin <paustin@eos.ubc.ca>
2020-01-21 21:38:31 -0800

    lab3 quiz fix

--- do_rsync.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash -v
-rsync -avz ../numlabs/* numlabs/.
-rsync -avz ../notebooks/lab1/* numeric_notebooks/lab1/.
-rsync -avz ../notebooks/lab2/* numeric_notebooks/lab2/.
-rsync -avz ../notebooks/lab3/* numeric_notebooks/lab3/.
