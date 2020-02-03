a295753bc9fd60b4c4a0b845e3c0151a4a7bad13
Phil Austin <paustin@eos.ubc.ca>
2020-02-03 08:50:14 -0800

    add vscode files

---
+++ demos/testvs.py
@@ -0,0 +1,11 @@
+# %%
+import context
+
+# %%
+from matplotlib import pyplot as plt
+fig, ax = plt.subplots(1,1)
+ax.plot([0,1],[0,1])
+print("stop me")
+ax.set_title("here is a title")
+plt.show()
+# %%
